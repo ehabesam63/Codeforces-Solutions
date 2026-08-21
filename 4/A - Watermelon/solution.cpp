36795fabb3f4767d8d4741830b2b296bc36c6dbb
Ehab Essam <ehabesam63@gmail.com>
2026-08-22 01:09:01 +0300

    Add Watermelon [A] from Codeforces

---
+++ 4/A - Watermelon/solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int w ;
+    cin >>w;
+    if (w<=100 && w>=1)
+    {
+ 
+        if (w>2&&w%2==0)
+        cout <<"YES"<<endl;
+        else
+        cout <<"NO"<<endl;
+ 
+    }
+ 
+    
+ 
+ 
+ 
+    return 0;
+}
