eaeec6fa08fe7e972160860f8c2fc41e19445ce3
Ehab Essam <ehabesam63@gmail.com>
2026-08-22 12:49:38 +0300

    Add Way Too Long Words [A] from Codeforces

---
+++ 71/A - Way Too Long Words/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin >>n;
+    string arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0; i<n;i++)
+    {
+        if (arr[i].length()>10)
+        {
+ 
+          cout <<(arr[i])[0]<<(arr[i]).length()-2<<(arr[i])[arr[i].length()-1]<<endl;
+        }
+        else
+            cout <<arr[i]<<endl;
+    }
+    return 0;
+}
