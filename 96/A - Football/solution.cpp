def3d2ca71a7617d60e74f4958e3825870b140a5
Ehab Essam <ehabesam63@gmail.com>
2026-09-05 16:09:15 +0300

    Add Football [A] from Codeforces

--- 96/A - Football/solution.cpp
+++ 96/A - Football/solution.cpp
@@ -6,20 +6,11 @@ int main()
 {
     string a;
     cin>>a;
-    string s ="NO";
-    if (a.length()>=7)
-    {
-        
-    for (size_t i =0;i<=a.length()-7;i++)
-    {
-        if(a[i]=='0'&&a[i+1]=='0'&&a[i+2]=='0'&&a[i+3]=='0'&&a[i+4]=='0'&&a[i+5]=='0'&&a[i+6]=='0'||a[i]=='1'&&a[i+1]=='1'&&a[i+2]=='1'&&a[i+3]=='1'&&a[i+4]=='1'&&a[i+5]=='1'&&a[i+6]=='1')
-          {
-              s="YES";
-              break;
-          }
-    }
-    
-    }
-    cout <<s;
+    if(a.find("0000000")!=string::npos||a.find("1111111")!=string::npos)
+        cout <<"YES
+";
+    else
+        cout <<"NO
+";
     return 0;
 }
